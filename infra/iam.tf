4537cdb1315018e310004ef263b802efed36d819
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2025-08-13 01:31:07 +0200

    Add EBS CSI driver policy attachment to EKS node group role

--- infra/iam.tf
+++ infra/iam.tf
@@ -23,8 +23,14 @@ resource "aws_iam_role_policy_attachment" "eks_worker_node_AmazonEKS_CNI_Policy"
 	role       = aws_iam_role.eks_node_group_role.name
 }
 
+
 resource "aws_iam_role_policy_attachment" "eks_worker_node_AmazonEC2ContainerRegistryReadOnly" {
 	policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+	role       = aws_iam_role.eks_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_AmazonEBSCSIDriverPolicy" {
+	policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 	role       = aws_iam_role.eks_node_group_role.name
 }
 
