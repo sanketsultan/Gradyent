1084c1063a4540e3f408403575de05d65b969bb6
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2025-08-12 23:49:05 +0200

    Add IAM role policy attachment for AmazonEKS_CNI_Policy to node group

--- infra/node-group-role.tf
+++ infra/node-group-role.tf
@@ -15,6 +15,11 @@ resource "aws_iam_role_policy_attachment" "node_group_AmazonEKSWorkerNodePolicy"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "node_group_AmazonEKS_CNI_Policy" {
+  role       = aws_iam_role.node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
 resource "aws_iam_role_policy_attachment" "node_group_AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.node_group.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
