7400477501536d585f0e8869494efeb6ca57dcfe
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2025-08-13 01:15:16 +0200

    Update IAM role policy attachments to use the correct node group role

--- infra/iam.tf
+++ infra/iam.tf
@@ -85,12 +85,13 @@ resource "aws_iam_user_policy" "gradyent_ssm_read" {
 }
 
 
+
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+	role       = aws_iam_role.eks_node_group_role.name
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+	role       = aws_iam_role.eks_node_group_role.name
+	policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
