f56f16c1519fb254d9e7018a5a87c5d709099ab9
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2025-08-13 20:59:38 +0200

    Refactor Kubernetes provider configuration for consistency in Datadog setup

--- infra/observability/datadog.tf
+++ infra/observability/datadog.tf
@@ -1,6 +1,12 @@
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "arn:aws:eks:eu-west-1:375459824176:cluster/gradyent-eks"
+}
+
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    config_path    = "~/.kube/config"
+    config_context = "arn:aws:eks:eu-west-1:375459824176:cluster/gradyent-eks"
   }
 }
 
