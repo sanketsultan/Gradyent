dd65f2bd52feddc6ea3e963e7abfc50d7b0d79ea
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2025-08-13 18:54:57 +0200

    Downgrade EKS module version from 21.0.6 to 20.13.0

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.0.6"
+  version = "20.13.0"
   name    = var.cluster_name
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
@@ -39,4 +39,8 @@ module "eks" {
       }
     }
   }
+
+  cluster_endpoint_public_access = true
+  cluster_endpoint_private_access = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 }
