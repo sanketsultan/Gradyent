0cd1c7bc78f1889478c01e9679f082bb12374381
Sanket Sultan <42866957+sanketsultan@users.noreply.github.com>
2025-08-14 20:16:32 +0200

    Refactor Velero S3 bucket configuration: update environment tag to 'dev' and ensure lifecycle prevent_destroy is set; remove unused IAM role and policy resources.

--- infra/velero.tf
+++ infra/velero.tf
@@ -1,9 +1,12 @@
 resource "aws_s3_bucket" "velero" {
   bucket = "gradyent-velero-backups"
   force_destroy = false
+  lifecycle {
+  prevent_destroy = true
+  }
   tags = {
     Name        = "Velero Backup Bucket"
-    Environment = "production"
+    Environment = "dev"
   }
 }
 
@@ -23,45 +26,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "velero" {
   }
 }
 
-resource "aws_iam_role" "velero" {
-  name = "velero"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_policy" "velero" {
-  name        = "velero-policy"
-  description = "Velero access to S3 for backups"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject",
-          "s3:ListBucket"
-        ]
-        Resource = [
-          aws_s3_bucket.velero.arn,
-          "${aws_s3_bucket.velero.arn}/*"
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "velero" {
-  role       = aws_iam_role.velero.name
-  policy_arn = aws_iam_policy.velero.arn
-}
 
 // create namespace
 resource "kubernetes_namespace" "velero" {
